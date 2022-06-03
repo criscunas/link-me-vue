971250ec5e2d02159d02268cc3977ea08285713a
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-06-03 15:01:07 -0400

    updated homepage styling

--- src/components/LandingPageThree.vue
+++ src/components/LandingPageThree.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="m-4">
     <div>
-      <h1 class="text-white text-4xl py-4">About us</h1>
-      <p class="text-white"> A small start up with the goal of helping you grow and expand your social following. Our product allows your profile visitors to easily 
+      <h1 class="text-white text-4xl pb-6">About us</h1>
+      <p class="text-white text-lg"> A small start up with the goal of helping you grow and expand your social following. Our product allows your profile visitors to easily 
       find you on any social platform. </p>      
     </div>
-  <div>
-    <h1 class="text-white text-4xl py-4">Contact</h1>
+  <div class="mt-6">
+    <h1 class="text-white text-4xl pt-4 pb-6">Contact</h1>
+    <div class="flex justify-evenly" >
+      <img class="h-12 w-auto" src = '../../public/images/github.svg' />
+      <img class="h-12 w-auto" src = '../../public/images/twitter.svg' />
+      <img class="h-12 w-auto" src = '../../public/images/email.png' />
+    </div>
   </div>
  </div>
 </template>
