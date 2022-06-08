41570d463be2574288d881ba89d7978a8d94bc55
Cristopher Cunas <cristophercunas@Cristophers-Computer.local>
2022-06-08 12:42:40 -0400

    setting up UI for forms, no functionality yet

---
+++ src/components/BioForm.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+  props: {
+    bio: String
+  },
+  data () {
+    return {
+      newBio : ''
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="px-2 py-4 mb-4 border rounded-md">
+    <p class="text-center" v-if = "!bio"> Your bio here.</p>
+    <p v-else> {{bio}} </p>
+  </div>
+
+  <div class="text-right">
+    <label for="bio-modal" class="dashboard-btn"> Edit Bio </label>    
+  </div>
+
+    <input type="checkbox" id="bio-modal" class="modal-toggle" />
+    <div class="modal modal-bottom sm:modal-middle">
+      <div class="modal-box relative">
+        <label for="bio-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+          <div class="mt-6">
+            <p class="main-text text-[1.2rem] text-black mb-4"> Update your Profile Bio</p>
+            <FormKit
+              type = "textarea"
+              v-model = "newBio"
+              rows = 3
+              :classes="{
+                inner: 'bio-inner',
+                input: 'bio-input'
+              }"
+            />
+            <button class="form-btn mt-4">
+              Submit
+            </button>
+          </div>
+      </div>
+  </div>
+</template>
+
+<style>
+  .bio-input {
+    resize: none;
+    width: 100%;
+    height: max-content;
+  }
+  .bio-inner {
+    border: 1px solid gray;
+    border-radius: 8px;
+    overflow: hidden;
+  }
+</style>
---
+++ src/components/ImageForm.vue
@@ -0,0 +1,45 @@
+<script>
+  export default {
+    data () {
+      return {
+        file: ''
+      }
+    }    
+  }
+</script>
+
+<template>
+
+  <div class="text-center">
+    <div class="avatar">
+      <div class="w-24 mask mask-squircle">
+        <img src="../../public/images/profile.png"/>
+      </div>
+    </div>
+  </div>
+
+  
+  <div class="flex mt-4 justify-center">
+    <FormKit 
+      type="file"
+      v-model = "file"
+      accept=".jpg,.png,.pdf"
+      :classes="{
+        label: 'dashboard-btn'
+      }"
+      label="Upload"
+    />
+  </div>
+  
+  
+</template>
+
+<style>
+  .formkit-no-files{
+    display: none;
+  }
+  input[type="file"] {
+    display: none;
+  }
+
+</style>
