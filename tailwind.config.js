c63f89a7136f2350dfc41ce08b5cd2516e57aa35
Cristopher Cunas <cristophercunas@icloud.com>
2022-06-11 22:55:38 -0400

    edited styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}', './views/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      'small' : '375px',
+      'small' : '400px',
+      'tablet' : '768px',
     },
   },
   plugins: [require("daisyui")],
