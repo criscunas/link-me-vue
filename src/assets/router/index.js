37dbd4a454f409317c92205ec0eb2a2fb786b264
Cristopher Cunas <cristophercunas@icloud.com>
2022-06-18 00:57:39 -0400

    added in new route

--- src/assets/router/index.js
+++ src/assets/router/index.js
@@ -1,17 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
-import DashBoard from '../../../views/DashBoard.vue'
-import LandingPages from '../../components/LandingPages.vue'
+import DashBoard from '../../../views/DashBoard.vue';
+import LandingPages from '../../components/LandingPages.vue';
+import UserProfile from '../../../views/UserProfile.vue';
 
 const routes = [
   {
     path:"/",
     name: 'Home',
     component: LandingPages
-  },
-  {
+  }, {
     path: '/dashboard',
     name: 'Dashboard',
     component : DashBoard
+  }, {
+    path: '/profile',
+    name: 'Profile',
+    component: UserProfile
   }
 ]
 
